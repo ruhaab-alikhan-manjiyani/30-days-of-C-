95b39c2a764260413ff12af7c5c7e0436c24b73e
Ruhaab Alikhan Manjiyani <137204777+ruhaab-alikhan-manjiyani@users.noreply.github.com>
2025-09-05 14:53:22 +0530

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout <<"My name is Ruhaab."<< endl;
+    cout<<"I am 17 years old."<<endl;
+    cout<<"I will master C++ in 30 days!"<<endl;
+    return 0;
+}
